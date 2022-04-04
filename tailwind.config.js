70ff1aa1218f51e906ce4421bb109d2426c392e3
Emre Badem <emrebadem_@hotmail.com>
2022-04-04 23:45:15 +0300

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    colors: {
+      "cmdr-outer-container": "#887ECB",
+      "cmdr-inner-container": "#50459B",
+      "cmdr-text-color": "#887ECB",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
