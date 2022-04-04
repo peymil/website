25d9157083d39ee08c10634d879c9ed4743a43ff
Emre Badem <emrebadem_@hotmail.com>
2022-04-04 23:47:34 +0300

    feat: add basic info page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-cmdr-outer-container flex h-full w-full items-center justify-center">
+      <div className="bg-cmdr-inner-container flex w-3/5 h-4/6">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
